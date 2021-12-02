0f6dd050dd9862b98d33c31506f0cb44c1743ec3
frank <frank@embleton.xyz>
2021-12-02 20:02:43 +0100

    day 2 solution

--- day_2/day_2.py
+++ day_2/day_2.py
@@ -1,3 +1,30 @@
 def load_data():
     with open('input.txt', 'r') as f:
         return [line.strip().split() for line in f]
+
+def part_1(data):
+    h, d = 0, 0
+    for direction, value in load_data():
+        if direction == 'forward':
+            h += int(value)
+        elif direction == 'down':
+            d += int(value)
+        elif direction == 'up':
+            d -= int(value)
+    return (h * d)
+
+print(f'Part 1: {part_1(load_data())}')
+
+def part_2(data):
+    a, h, d = 0, 0, 0
+    for direction, value in data:
+        if direction == 'down':
+            a += int(value)
+        elif direction == 'up':
+            a -= int(value)
+        elif direction == 'forward':
+            h += int(value)
+            d += a * int(value)
+    return (h * d)
+
+print(f'Part 2: {part_2(load_data())}')
